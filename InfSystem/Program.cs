013bf1e4995d4c755a2f23a811d2016f3a76fb83
Кирилл <kirillbolbot546@gmail.com>
2026-09-10 14:44:55 +0700

    Практика 1

--- InfSystem/Program.cs
+++ InfSystem/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace InfSystem
 {
@@ -10,6 +11,13 @@ namespace InfSystem
     {
         static void Main(string[] args)
         {
+            
+            Console.WriteLine("Измерение температуры\nВведите свои параметры (дата, место измерения и значение)");
+            string input = Console.ReadLine();
+            Weather weather = new Weather();
+            weather.FromStr(input);
+            string[] lines = { $"{weather.Date}\n{weather.Place}\n{weather.TemperatureValue}" };
+            File.WriteAllLines("file.txt", lines);
         }
     }
 }
---
+++ InfSystem/Weather.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfSystem
+{
+    internal class Weather
+    {
+        public DateTime Date {  get; set; }
+        public string Place {  get; set; }
+        public float TemperatureValue {  get; set; }
+
+        public Weather()
+        {
+
+        }
+        public void FromStr(string f)
+        {
+            string[] entry = f.Trim().Split(' ');
+            Date = DateTime.Parse(entry[0]);
+            Place = entry[1];
+            TemperatureValue = float.Parse(entry[2]);
+        }
+    }
+}
